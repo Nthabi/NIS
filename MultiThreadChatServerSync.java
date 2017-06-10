e123c68093ab401055eedc7f77ffd9b464c46e20
sonimj <sonimj@users.noreply.github.com>
2017-06-10 19:53:49 +0200

    Add files via upload

---
+++ MultiThreadChatServerSync.java
@@ -0,0 +1,220 @@
+package rsa;
+
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.security.*;
+import java.net.ServerSocket;
+/*
+*A chat server that delivers public and private messages.
+*/
+
+
+public class MultiThreadChatServerSync{
+	//server's public key 
+	
+	static File pub_key = new File("public.key"); //name of public key file
+	static File prv_key = new File("private.key"); //name of private key file
+	//file to store public keys
+	static File public_keys = new File("keys.public");
+	static RSA rsa = new RSA();
+   //the server socket
+   private static ServerSocket serverSocket=null;
+   //The client socket
+   private static Socket clientSocket=null;
+   //This chat server can accept up to maxClientsCount' connections.
+   private static final int maxClientsCount=10;
+   private static final clientThread [] threads=new clientThread[maxClientsCount];
+   
+   //generate public and private keys for server
+   private static final KeyPair keyPair = rsa.rsaKeyGen();
+   public static PublicKey publicKey = keyPair.getPublic();
+   private static PrivateKey privateKey = keyPair.getPrivate();
+
+   public static void main(String [] args){
+
+	   try {
+	    	    
+		   /** **DEBUG
+		    System.out.println(publicKey);
+			System.out.println(privateKey);*/
+		   
+		   MultiThreadChatClient.serverPubKey = publicKey;
+	    	  
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	   
+      //the default port number;
+      int portNumber=2040;
+      if(args.length <1){
+         System.out.println("Connection established using port number = "+portNumber);
+      }
+      else{
+         portNumber=Integer.valueOf(args[0]).intValue();
+      }
+      
+      /*
+   *Open a server socket on the portNumber(default 222). Note that we can
+   *not choose a port less than 1023 if we are not privileged usera(root)
+   */
+   try{
+      serverSocket=new ServerSocket(portNumber);
+      
+   }catch(IOException e){
+      System.out.println(e);
+   }
+   /*
+   *Create a client socket for each connection and pass it to a new client
+   *thread
+   */
+   while(true){
+      try{
+         clientSocket=serverSocket.accept();
+         int i=0;
+         for( i=0; i<maxClientsCount;i++){
+            if(threads[i]==null){
+               (threads[i]=new clientThread(clientSocket,threads)).start();
+               break;
+            }
+         }
+         if(i==maxClientsCount){
+            PrintStream os=new PrintStream(clientSocket.getOutputStream());
+            os.println("Server too busy.Try later");
+            os.close();
+            clientSocket.close();
+         }
+      }catch(IOException e){
+         System.out.println(e);
+      }
+   }
+   }
+}
+
+/*
+*The chat client thread. this client thread opnes the input and the output
+* streams for a particular client, asks the client's name , informs all the
+*Clients connectred to the server about the the fact that a new client has joined
+*the chat room, and as long as it recieve data, echoes that data back to all
+* other clients. When a client leaves the chat room this thread informs
+*also all other clients about that and terminates.
+*/
+class clientThread extends Thread{
+   private String clientName = null;
+   private DataInputStream is=null;
+   private PrintStream os =null;
+   private Socket clientSocket=null;
+   private final clientThread [] threads;
+   private int maxClientsCount;
+
+
+   
+   public clientThread(Socket clientSocket,clientThread [] threads){
+      this.clientSocket=clientSocket;
+      this.threads=threads;
+      maxClientsCount=threads.length;
+
+   }
+
+   @SuppressWarnings("deprecation")
+public void run(){
+      int maxClientscount=this.maxClientsCount;
+      clientThread [] threads=this.threads;
+      try{
+           /*
+         *Create input and output streams for this client.
+         */
+         is=new DataInputStream(clientSocket.getInputStream());
+         os=new PrintStream(clientSocket.getOutputStream());
+         String name;
+         while(true){
+            os.println("Enter your name");
+            name=is.readLine().trim();
+            if(name.indexOf('@')==-1){
+               break;
+            }
+            else{
+               os.println("The name should not contain '@' character.");
+            }
+            //authentication protocol here...
+           
+         }
+         /*Welcome the new client.*/
+         os.println(" You have been connected");
+         synchronized(this){
+            for(int i=0;i<maxClientsCount;i++){
+               if(threads[i] !=null && threads [i] ==this){
+                  clientName=name;
+                  break;
+               }
+            }
+            for(int i=0;i<maxClientsCount;i++){
+               if(threads[i] !=null && threads[i] != this){
+                  threads[i].os.println("***" + name + " connected on the server ***");
+               }
+            }
+         }
+try {
+         /*Start conversation.*/
+	 encryptor object = new encryptor();
+         while(true){
+            String line=is.readLine();
+	    byte[] initVector = object.initialisationVector();
+	    String siseko = object.encrypt(line, initVector, "athabhazxcvbnmas");
+            if(line.startsWith("/quit")){
+               break;
+            }
+            /*Send private message to  client.*/
+               /*
+              The message is public, broadcast it to all other clients.
+              */
+              synchronized(this){
+                 for(int i=0; i<maxClientsCount; i++){
+                    if(threads[i] != null && threads[i].clientName != null){
+			if (threads[i] != this) {
+				threads[i].os.println("<"+name + "> "+ siseko);
+				String decr = object.decrypt(siseko, initVector, "athabhazxcvbnmas");
+                       		threads[i].os.println("<"+name + "> "+ decr);
+			}
+                    }
+                 }
+              }
+        
+         }
+} catch(Exception e) { }
+
+	 /*
+         synchronized(this){
+            for(int i=0; i<maxClientsCount;i++){
+               if(threads[i] != null && threads [i] != this && threads[i].clientName !=null){
+                  threads[i].os.println("*** The user "+ name + " has disconnected ***");
+               }
+            }
+         }*/
+
+         os.println("*** Bye "+ name + " ****");
+         /*
+         *Clean up. Set the current thread variable to null so that a new client
+         *could be accepted by the server.
+         */
+         synchronized(this){
+            for(int i=0; i<maxClientsCount; i++){
+               if(threads[i]==this){
+                  threads[i]=null;
+               }
+            }
+         }
+         /*Close the output stream, close the input stream, close the socket*/
+         is.close();
+         os.close();
+         clientSocket.close();
+
+      }
+      catch(IOException e){
+
+      }
+   }
+}
--- RSA.java
+++ RSA.java
@@ -8,58 +8,27 @@ import javax.crypto.*;
 
 public class RSA {
 	//Generates key pairs and stores them in specified files. Keys stored separately
-	public void rsaKeyGen(String file_public, String file_private){
-		//byte[] keyBytes;
+	public KeyPair rsaKeyGen(){
+		KeyPair keyPair = null;
 		try{
 			
 			KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
 			keyPairGen.initialize(2048);
-			KeyPair keyPair = keyPairGen.generateKeyPair();
-			PublicKey publicKey = keyPair.getPublic();
-			PrivateKey privateKey = keyPair.getPrivate();
-			
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA"); //converts keys into keyspecifications into their modulus and exponent constituents
-			RSAPublicKeySpec rsaPublic = keyFactory.getKeySpec(publicKey, RSAPublicKeySpec.class);
-			RSAPrivateKeySpec rsaPrivate = keyFactory.getKeySpec(privateKey, RSAPrivateKeySpec.class);
-			
-			storeKeys(file_public, rsaPublic.getModulus(), rsaPublic.getPublicExponent()); //public key stored as 2 objects: modulus and exponent
-			storeKeys(file_private, rsaPrivate.getModulus(), rsaPrivate.getPrivateExponent());		
+			keyPair = keyPairGen.generateKeyPair();
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
+		return keyPair;
 	}
 	
-	//Stores key in specified file. 2 objects stored - modulus and exponent which make up the key
-	void storeKeys(String fname, BigInteger mod, BigInteger exp) throws IOException{
-		
-		try{
-			File file = new File(fname);
-			if(!file.exists()){
-				file.createNewFile();
-			}
-			FileOutputStream fos = new FileOutputStream(file);
-			ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(fos));
-			
-			oos.writeObject(mod);
-			oos.writeObject(exp);
-			
-			oos.flush();
-			oos.close();
-			fos.flush();
-			fos.close();
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
 	
 	//for encryption using public key
-	byte[] encrypt(String data, String fname){
+	byte[] encrypt(String data, PublicKey publicKey){
 		
 		byte[] encrypted = null;
 		try{
-			PublicKey publicKey = getPublicKey(fname); //regenerates key and returns it
+			//PublicKey publicKey = getPublicKey(fname); //regenerates key and returns it
 			Cipher cipher = Cipher.getInstance("RSA");
 			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
 			encrypted = cipher.doFinal(data.getBytes());
@@ -73,11 +42,11 @@ public class RSA {
 		return encrypted;
 	}
 	
-	//for encryption
-	String decrypt(byte[] data, String fname) throws IOException{
+	//for dencryption
+	String decrypt(byte[] data, PrivateKey privateKey){
 		String decrypted = null;
 		try{
-			PrivateKey privateKey = getPrivateKey(fname); //regenerates key and returns it
+			//PrivateKey privateKey = getPrivateKey(fname); //regenerates key and returns it
 			Cipher cipher = Cipher.getInstance("RSA");
 			cipher.init(Cipher.DECRYPT_MODE, privateKey);
 			byte[] decrByte = cipher.doFinal(data);
@@ -88,45 +57,5 @@ public class RSA {
 			e.printStackTrace();
 		}
 		return decrypted;	
-	}
-	
-	PublicKey getPublicKey(String fname) throws IOException{
-		PublicKey publicKey = null;
-		
-		try{
-			FileInputStream fis = new FileInputStream(fname);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-		
-			RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec((BigInteger) ois.readObject(), (BigInteger) ois.readObject()); //(modulus, exponent)
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			publicKey = keyFactory.generatePublic(rsaPublicKeySpec);
-			
-			fis.close();
-			ois.close();
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		return publicKey;
-	}
-	
-	PrivateKey getPrivateKey(String fname) throws IOException{
-		PrivateKey privateKey = null;
-		
-		try{
-			FileInputStream fis = new FileInputStream(fname);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-		
-			RSAPrivateKeySpec rsaPrivateKeySpec = new RSAPrivateKeySpec((BigInteger) ois.readObject(), (BigInteger) ois.readObject()); //(modulus, exponent)
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			privateKey = keyFactory.generatePrivate(rsaPrivateKeySpec);
-			
-			fis.close();
-			ois.close();
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		return privateKey;
 	}
 }
